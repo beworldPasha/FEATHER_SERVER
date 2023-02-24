01bd9175ba3cad800fbfb00974eadc87d7a5f3f4
beworld.pasha <beworld.pasha@yandex.ru>
2023-02-24 20:56:14 +0300

    Try to fix null data source

--- app/src/main/java/com/example/appapi/MainActivity.java
+++ app/src/main/java/com/example/appapi/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.appapi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.feather.*;
 
+
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,10 +34,43 @@ public class MainActivity extends AppCompatActivity {
 
         buttonStart = findViewById(R.id.button);
         buttonStart.setOnClickListener(listener);
+
     }
 
     View.OnClickListener listener = view -> {
         if (editor.getText().toString().equals("")) return;
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        FeatherAPI.getInstance().startSession("84.246.85.148", 8000);
+
+        String data = editor.getText().toString();
+
+        Song song = FeatherAPI.getInstance().fetchData(data, Song.class);
+
+        songName.setText(song.name);
+        artistName.setText(song.artist);
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
+
+        try {
+            mediaPlayer.setAudioAttributes(
+                    new AudioAttributes.Builder()
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
+                            .build()
+            );
+            mediaPlayer.setDataSource(song.url);
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        FeatherAPI.getInstance().closeSession();
     };
 
 
