ed10294d9e1e7bd59067d80c72cfc3c4d7c1c8b1
beworld.pasha <beworld.pasha@yandex.ru>
2023-02-24 14:35:29 +0300

    Add widgets & button listener

--- app/src/main/java/com/example/appapi/MainActivity.java
+++ app/src/main/java/com/example/appapi/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.appapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static ImageView imageView;
+    private static TextView songName;
+    private static TextView artistName;
+    private static EditText editor;
+    private static Button buttonStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.song_image);
+        songName = findViewById(R.id.song_name);
+        artistName = findViewById(R.id.artist_name);
+        editor = findViewById(R.id.editor);
+
+        buttonStart = findViewById(R.id.button);
+        buttonStart.setOnClickListener(listener);
     }
+
+    View.OnClickListener listener = view -> {
+        if (editor.getText().toString().equals("")) return;
+    };
+
+
 }
