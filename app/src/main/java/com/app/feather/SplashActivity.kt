bb425e003da58ae17d837b3938607f2620869468
beworld.pasha <beworld.pasha@yandex.ru>
2023-05-24 21:15:18 +0300

    Добавлен метод startSession, отвечающий за проверку на выполненный вход с запоминанием

--- app/src/main/java/com/app/feather/SplashActivity.kt
+++ app/src/main/java/com/app/feather/SplashActivity.kt
@@ -10,15 +10,33 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
     }
 
     override fun onResume() {
         super.onResume()
-        startActivity(Intent(this, AuthorizationActivity::class.java))
+        startSession()
+        //startActivity(Intent(this, AuthorizationActivity::class.java))
     }
 
     override fun onStop() {
         super.onStop()
         finish()
+    }
+
+    private fun startSession() {
+        val preferenceManager = SharedPreferencesManager(this)
+        if (preferenceManager.isRemembered() == true) {
+            val accessToken = AccountsManager(applicationContext).
+            getAccessToken(preferenceManager.getUserLogin())
+
+            if (accessToken?.let { JWTManager().isAccessExpired(it) } == true) {
+                APIManager(applicationContext).refreshTokens()
+            }
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            startActivity(Intent(this, AuthorizationActivity::class.java))
+        }
     }
 }
